848fa1b869b4321042012d6ceb6abd97bf647442
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-05-18 22:43:03 +0900

    Assignment
    Java Class Exercise Quiz 2021.05.18

---
+++ Assignment/Quiz2.java
@@ -0,0 +1,10 @@
+package Assignment;
+
+public class Quiz2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TTTTTTTTTTT\nTTTTTTTTTT\n    TT    \n    TT    \n    TT    ");
+	}
+
+}
