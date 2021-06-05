0e97cf20cf803b210fae8d0c39561d8a84f67547
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 23:08:31 +0900

    Update Quiz3.java

--- ConditionExercise/Quiz3.java
+++ ConditionExercise/Quiz3.java
@@ -3,32 +3,33 @@ package ConditionExercise;
 import java.util.Scanner;
 
 public class Quiz3 
-
 {
 
 	public static void main(String[] args) 
-	
 	{
 		
 		Scanner input = new Scanner (System.in);
-		System.out.print("Number? (1 or 2 or 3): ");
+		System.out.print( "Number? (1 or 2 or 3): " );
 		int animal = input.nextInt();
 		
-		if (animal == 1)
-		{ System.out.println("dog");
+		if ( animal == 1 )
+		{ 
+			System.out.println("dog");
 		}
-		else if(animal == 2)
-		{ System.out.println("cat");
+		else if ( animal == 2 )
+		{ 
+			System.out.println("cat");
 		}
-		else if (animal == 3)
-		{ System.out.println("chick");
+		else if ( animal == 3 )
+		{ 
+			System.out.println("chick");
 		}
 		else
-		{ System.out.println("I don't know.");
+		{ 
+			System.out.println("I don't know.");
 		}
 
 		input.close();
-		
 	}
 
 }
