082f152dc964da7a5cd0833edf57dbe5143ffb5e
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 22:58:26 +0900

    Update Quiz1.java

--- Assignment/Quiz1.java
+++ Assignment/Quiz1.java
@@ -1,16 +1,17 @@
 package Assignment;
 
-public class Quiz1 {
+public class Quiz1 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		int x = 90;
 		int y = 80;
 		int z = 100;
-		int sum = (x + y + z);
+		int sum = ( x + y + z );
 		int avg = (sum) / 3;
 		
-		System.out.println("kor " + x + "\nmat " + y + "\neng " + z + "\nsum " + sum + "\navg " + avg);
+		System.out.println( "kor " + x + "\nmat " + y + "\neng " + z + "\nsum " + sum + "\navg " + avg );
 	}
 
 }
