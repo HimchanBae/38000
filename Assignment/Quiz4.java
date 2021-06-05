1ad381b885833095cb10f28e812bf24c4126e263
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 23:00:27 +0900

    Update Quiz4.java

--- Assignment/Quiz4.java
+++ Assignment/Quiz4.java
@@ -2,23 +2,24 @@ package Assignment;
 
 import java.util.Scanner;
 
-public class Quiz4 {
+public class Quiz4 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner (System.in);
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
 
-		System.out.print("첫번째 숫자를 입력하시오:");
+		System.out.print( "첫번째 숫자를 입력하시오:" );
 		int x = input.nextInt();
-		System.out.print("두번째 숫자를 입력하시오:");
+		System.out.print( "두번째 숫자를 입력하시오:" );
 		int y = input.nextInt();
-		System.out.print("세번째 숫자를 입력하시오:");
+		System.out.print( "세번째 숫자를 입력하시오:" );
 		int z = input.nextInt();
 		
 		int sum = x + y + z;
 		int avg = sum / 3;
 		
-		System.out.println("sum = " + sum + "\navg = " + avg);
+		System.out.println( "sum = " + sum + "\navg = " + avg );
 		
 	}
 
