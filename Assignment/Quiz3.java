2818ef2fa13006359b0b4575b96f23c59feab598
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 22:59:34 +0900

    Update Quiz3.java

--- Assignment/Quiz3.java
+++ Assignment/Quiz3.java
@@ -1,14 +1,15 @@
 package Assignment;
 
-public class Quiz3 {
+public class Quiz3 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		int X = 10;
 		int Y = 20;
 		int Z = X + Y;
 		
-		System.out.println(X + " + " + Y + " = " + Z);
+		System.out.println( X + " + " + Y + " = " + Z );
 	}
 
 }
