a9ac22c3dc3715ab53f8e37a875b6662ea0741f2
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 23:06:46 +0900

    Update Quiz1.java

--- ConditionExercise/Quiz1.java
+++ ConditionExercise/Quiz1.java
@@ -7,11 +7,9 @@ public class Quiz1
 	
 	public static void main(String[] args) 
 	
-	{
-
-		//사용자에게 필요한 정보를 입력 받음(정수) 
+	{	//사용자에게 필요한 정보를 입력 받음(정수) 
 		Scanner input = new Scanner(System.in);
-		System.out.print("정수를 입력해 주세요 : ");
+		System.out.print( "정수를 입력해 주세요 : " );
 		int n = input.nextInt();
 		
 		// 양수, 0, 음수 판단 조건문 작성
@@ -28,9 +26,7 @@ public class Quiz1
 			System.out.println( "minus");
 		}
 		
-		
 		input.close();
-		
 	}
 
 }
