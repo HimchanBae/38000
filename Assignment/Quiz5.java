b34f2ade8ede5fc5fb9a68f68d57ed04d09d3b54
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 23:01:15 +0900

    Update Quiz5.java

--- Assignment/Quiz5.java
+++ Assignment/Quiz5.java
@@ -2,19 +2,21 @@ package Assignment;
 
 import java.util.Scanner;
 
-public class Quiz5 {
+public class Quiz5 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("yard? ");
+		System.out.print( "yard? " );
 		double x = input.nextDouble();
 		double y = x * 91.44;
-		double remain = (y - (int)y) * 10;
-		int z = (int)(0.5 + remain);
+		double remain = ( y - (int)y ) * 10;
+		int z = (int)( 0.5 + remain );
 		
-		System.out.print(x + "yard = " + (int) y + "." + z + "cm");
+		System.out.print( x + "yard = " + (int) y + "." + z + "cm" );
+		
 	}
 
 }
