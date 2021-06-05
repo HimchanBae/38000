58cb5ec1ee97e3ce9fa455113b13376f998510ec
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 23:14:39 +0900

    Update Quiz6.java

--- ConditionExercise/Quiz6.java
+++ ConditionExercise/Quiz6.java
@@ -6,23 +6,18 @@ public class Quiz6
 {
 
 	public static void main(String[] args) 
-	{
-		
-		//년도를 입력 받음 
+	{	//연도를 입력 받음 
 		Scanner input = new Scanner(System.in);
-		System.out.print("연도를 입력해 주세요 : ");
-		
+		System.out.print( "연도를 입력해 주세요 : " );
 		int year = input.nextInt();
-		
 		//윤년 평년 판단 
 		if (( year%400 == 0 ) || ( year%4 == 0 && year%100 != 0 ))
 		{
-		System.out.print( "leap year" );
+			System.out.print( "leap year" );
 		}
-		
 		else
 		{
-		System.out.print( "common year" );	
+			System.out.print( "common year" );	
 		}
 		
 		input.close();
