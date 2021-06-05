8a7616afaf040e5243def53ebb424cf98b14636b
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 23:03:19 +0900

    Update Quiz3.java

--- AssignmentDay2/Quiz3.java
+++ AssignmentDay2/Quiz3.java
@@ -2,10 +2,11 @@ package AssignmentDay2;
 
 import java.util.Scanner;
 
-public class Quiz3 {
+public class Quiz3 
+{
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
 		Scanner input = new Scanner(System.in);
 		System.out.print("첫 번째 정수를 입력하시오 : ");
 		int x = input.nextInt();
@@ -16,13 +17,12 @@ public class Quiz3 {
 		int a = x++;
 		int b = --y;
 		
-		//줄이 넘어가서 x y 가 증감연산 된 후의 수치로 표시됨 + 연산은 변수 사용했으므로 원래로 돌아갔
+		//줄이 넘어가서 x y 가 증감연산 된 후의 수치로 표시됨 + 연산은 변수 사용했으므로 원래로 돌아갔음
 		System.out.print( x + " " );
 		System.out.print( y + " " );
 		System.out.println( a * b );
 		
 		input.close();
-		
 	}
 
 }
