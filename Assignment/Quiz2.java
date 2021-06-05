59b88f8ad360c17af5132c13b857c0d42d1c363f
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-05 22:59:02 +0900

    Update Quiz2.java

--- Assignment/Quiz2.java
+++ Assignment/Quiz2.java
@@ -1,9 +1,10 @@
 package Assignment;
 
-public class Quiz2 {
+public class Quiz2 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 		System.out.println("TTTTTTTTTTT\nTTTTTTTTTT\n    TT    \n    TT    \n    TT    ");
 	}
 
