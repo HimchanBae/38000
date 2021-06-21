7e585c5427e91d31cceeb8e0d26e2fc323d27402
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-21 21:25:00 +0900

    Update Quiz10.java

--- Assignment_Day5_String/Quiz10.java
+++ Assignment_Day5_String/Quiz10.java
@@ -10,11 +10,16 @@ public class Quiz10
 		Scanner in = new Scanner (System.in);
 		System.out.print("문장을 입력해 주세요 : ");
 		String text = in.nextLine(); //중요!! nextLine을 써야함...
-		
+		int len = text.length();
 		String[] words = text.split(" ");
 		
 		for(int i=0 ; i < words.length; i++) 
 		{
+			if(len > 100)
+			{
+				System.out.println("100자 이하로 입력해 주세요.");
+				break;
+			}
 			if(i%2 == 0) 
 			{
 				System.out.println(words[i]);
