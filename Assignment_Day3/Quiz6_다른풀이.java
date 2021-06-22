ce8280dc1e87bfc3a670852b95825cc92db13e29
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-22 11:57:54 +0900

    Update Quiz6_다른풀이.java

--- Assignment_Day3/Quiz6_다른풀이.java
+++ Assignment_Day3/Quiz6_다른풀이.java
@@ -8,24 +8,21 @@ public class Quiz6_다른풀이
 	public static void main(String[] args) 
 	{
 		char answer;
+		Scanner input = new Scanner ( System.in );
 		
 		do	
 		{
-			Scanner input = new Scanner ( System.in );
 			System.out.print("Base = ");
 			double base = input.nextDouble();
 			
-			Scanner input2 = new Scanner ( System.in );
 			System.out.print("Height = ");
-			double height = input2.nextDouble();
+			double height = input.nextDouble();
 			
 			double width = ( base * height ) / 2;
-					
 			System.out.printf("Triangle width = " + "%.1f" , width );
 			
-			Scanner input3 = new Scanner ( System.in );
 			System.out.print("\nContinue? ");
-			answer = input3.next().charAt(0);
+			answer = input.next().charAt(0);
 		
 		} while (( answer == 'y' ) || ( answer == 'Y' ));
 		
