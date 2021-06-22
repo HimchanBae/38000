90b568b0cc90b0a1c3c9969d5006a4fec6d97568
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-22 11:57:18 +0900

    Update Quiz6.java

--- Assignment_Day3/Quiz6.java
+++ Assignment_Day3/Quiz6.java
@@ -7,23 +7,22 @@ public class Quiz5
 
 	public static void main(String[] args) 
 	{	
-
-		while (true) //루프를 만들기 위해서?
+		Scanner input = new Scanner ( System.in );
+		
+		while (true)
 		{	//밑변과 높이를 입력 받음 
-			Scanner input = new Scanner ( System.in );
+			
 			System.out.print("Base = ");
 			double base = input.nextDouble();
 			
-			Scanner input2 = new Scanner ( System.in );
 			System.out.print("Height = ");
-			double height = input2.nextDouble();
+			double height = input.nextDouble();
 			
 			double width = ( base * height ) / 2; //넓이 계산
 			System.out.printf("Triangle width = " + "%.1f" , width ); //계산의 결과 값을 소수점 첫째자리까지 출력 	
 
-			Scanner input3 = new Scanner ( System.in ); //계속할지 물어봄 
 			System.out.print("\nContinue? ");
-			char answer = input3.next().charAt(0);
+			char answer = input.next().charAt(0);
 				//답변이 y or Y 라면 처음으로 돌아가서 반복  
 				if ( ( answer == 'y' ) || ( answer == 'Y' ) ) 
 				{
@@ -36,8 +35,6 @@ public class Quiz5
 				}
 
 			input.close();
-			input2.close();
-			input3.close();
 		}
 
 	}
