a74b0dd7d3e57dac6469423b0259a54ab7a3b2b6
HimchanBae <84004506+HimchanBae@users.noreply.github.com>
2021-06-28 21:28:53 +0900

    Update Quiz7.java

--- Assignment_Day5_String/Quiz7.java
+++ Assignment_Day5_String/Quiz7.java
@@ -12,23 +12,22 @@ public class Quiz7
 		String str = in.next();
 		char[] arr = new char[100];
 		
-		for (int i=0; i<100; i++)
+		for (int i=0; i<str.length(); i++)
 		{
 			arr[i] = str.charAt(i);
 			
-		    if (arr[i] >= 'a' && arr[i] <= 'z' || arr[i] >= 'A' && arr[i] <= 'Z' || arr[i] >= '0' && arr[i] <= '9') 
+		    if ( ((arr[i] >= 'a') && (arr[i] <= 'z')) || ((arr[i] >= 'A') && (arr[i] <= 'Z')) || ((arr[i] >= '0') && (arr[i] <= '9')) ) 
 		    {
-		    	if (arr[i] >= 'A' && arr[i] <= 'Z') 
+		    	if ((arr[i] >= 'A') && (arr[i] <= 'Z')) 
 		    	{
-                    System.out.print(String.valueOf(arr[i]).toLowerCase());
-                } 
+                		System.out.print(String.valueOf(arr[i]).toLowerCase());
+              		} 
 		    	else 
 		    	{
 		    		System.out.print(arr[i]);
 		    	}
 		    }
 		}
-		System.out.println(str.toLowerCase());
 		in.close();
 	}
 	
